61b0794ea32230b49d5bbb65be53c2b43f6d9772
zubenko <a.zubenko7@gmail.com>
2014-11-20 01:06:31 +0200

    why

---
+++ academy/array.js
@@ -0,0 +1,42 @@
+var text = "ersdtyfgafdhgdasdasdxcvvzxvzXbaaaaaandbsandb";
+var myName = "Alex";
+var hits = [];
+
+for ( i=0; i<text.length; i++) {
+    if (text[i] === "a") {
+        hits.push(text[i]);
+
+    }
+}
+hits.length
+
+
+
+
+
+        /*
+        for (j=i; j < myName.length + i; j++) {
+            hits.push(text[j]+ "#");
+        }
+    }
+}
+if (hits === 0) {
+    console.log("Your name wasn't found!");
+}else {
+    console.log (hits);
+}*/
+
+var text = [1,2,3,4,5];
+for (i = text.length - 1; i >= 0; i--) {
+    console.log(text[i]);
+}
+var some = [];
+var numb = 1;
+do { some.push("what a fuck")
+}
+while (numb === 3) {
+    console.log (some);
+    numb++;
+
+
+}
---
+++ academy/why.js
@@ -0,0 +1,14 @@
+var text = [1,2,3,4,5];
+for (i = text.length - 1; i >= 0; i--) {
+    console.log(text[i]);
+}
+var some = [];
+var numb = 1;
+do { some.push("what a fuck")
+}
+while (numb === 3) {
+    console.log (some);
+    numb++;
+
+
+}
