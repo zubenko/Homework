9a83941016fc599153a9f1ce53ba07b72d150bac
zubenko <a.zubenko7@gmail.com>
2014-11-23 02:47:02 +0200

    dragon2 alomst finished

--- academy/dragon2.js
+++ academy/dragon2.js
@@ -7,26 +7,29 @@ var totalDamage = 0;
 var dragonHealth = 10;
 
 while (slaying) {
+    totalDamage += damageThisRound;
+    dragonHealth -= damageThisRound;
     if (youHit) {
         console.log("You hit the dragon and did " + damageThisRound + " damage!");
-        totalDamage += damageThisRound;
-        dragonHealth -= totalDamage;
+
         console.log ("The dragon health equal: " + dragonHealth + " !!! ");
 
-        if ( totalDamage >= 10 ) {
+        if ( dragonHealth <= 0 ) {
             console.log("You did it! You slew the dragon!!!");
             slaying = false;
-        } else {
-            youHit = Math.floor(Math.random() * 2);
-
-        }
-         if (youHit) {
-            console.log("You dodged and have a chance to hit again!!!");
         }
         else {
-             console.log ("Dragon riposte and made FATALITY :( You lose!!!");
-         }
-    } else {
+            youHit = Math.floor(Math.random() * 2);
+
+            if (youHit) {
+                console.log("You dodged and have a chance to hit again!!!");
+            }
+            else {
+                console.log("Dragon riposte and made FATALITY :( You lose!!!");
+            }
+        }
+    }
+    else {
         console.log("The dragon burninates you! You're toast.");
         slaying = false;
     }
