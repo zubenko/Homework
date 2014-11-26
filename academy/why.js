1569537512e8ae22c94414a8e083ad6f5a42624d
zubenko <a.zubenko7@gmail.com>
2014-11-27 01:16:37 +0200

    hz

--- academy/why.js
+++ academy/why.js
@@ -1,14 +1,17 @@
-var text = [1,2,3,4,5];
-for (i = text.length - 1; i >= 0; i--) {
-    console.log(text[i]);
-}
-var some = [];
-var numb = 1;
-do { some.push("what a fuck")
-}
-while (numb === 3) {
-    console.log (some);
-    numb++;
 
+var isEven = function(number)
+{
+    if (number % 2 === 0)
+    {
+        return true;
+    }
+    if else ( isNan(number))
+    {
+        return "it is a number";
+    }
+    else
+    {
+        return false;
+    }
 
-}
+};
